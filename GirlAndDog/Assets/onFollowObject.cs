9d5df582bf54b0e9b448c4bc95c8fe1741177bfa
aszx2526 <tsai2526@hotmail.com>
2016-12-30 10:55:04 +0800

    20161230_01
    把專案資源丟進來
    =============
    撰寫背包功能
    實作項目：
    使用xbox搖桿的十字方向鍵的左右
    進行使用道具選擇
    被選到的道具會放大，沒被選到的會縮小
    
    閒置10秒，包包會淡出下收
    按下十字方向鍵的左或右時
    包包會從底部向上淡入滑出

---
+++ GirlAndDog/Assets/onFollowObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onFollowObject : MonoBehaviour {
+    public GameObject myTarget;
+    public float myFollowSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, myTarget.transform.position, Time.deltaTime * myFollowSpeed);
+	}
+}
