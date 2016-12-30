a9da6154db600953f9b7a201aa85e82599404094
aszx2526 <tsai2526@hotmail.com>
2016-12-30 12:46:29 +0800

    20161230_04
    增加LB RB切換攝影機

--- GirlAndDog/Assets/Scripts/onFollowObject.cs
+++ GirlAndDog/Assets/Scripts/onFollowObject.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class onFollowObject : MonoBehaviour {
-    public GameObject myTarget;
+    public GameObject[] myTarget;
+    public int myWatchMod;
     public float myFollowSpeed;
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,8 @@ public class onFollowObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.Lerp(transform.position, myTarget.transform.position, Time.deltaTime * myFollowSpeed);
-	}
+        transform.position = Vector3.Lerp(transform.position, myTarget[myWatchMod].transform.position, Time.deltaTime * myFollowSpeed);
+        if (Input.GetButton("LB")) { myWatchMod = 0; }
+        if (Input.GetButton("RB")) { myWatchMod = 1; }
+    }
 }
